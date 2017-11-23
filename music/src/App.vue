a076a308c19ebedf68e0bb7ba1d4de65e7e14ae7
zhangsanfeng <123456@qq.com>
2017-11-23 11:05:08 +0800

    项目架构初始化

--- music/src/App.vue
+++ music/src/App.vue
@@ -1,8 +1,16 @@
 <template>
+    <div id="App">123</div>
 </template>
 
 <script type="text/ecmascript-6">
+    export default{
+        name: 'app'
+    }
 </script>
 
-<style lang="less">
+<style lang="less" rel="stylesheet/less">
+    body {
+        background: red;
+        display: flex;
+    }
 </style>
--- music/src/main.js
+++ music/src/main.js
@@ -1,3 +1,17 @@
 /**
  * Created by sf on 2017/11/23.
  */
+
+import Vue from 'vue';
+
+import App from './App.vue';
+
+// import '../src/common/less/index.less';
+
+new Vue({
+    el: '#app',
+    render: (function (h) {
+        return h(App)
+    })
+    // render:h=>h(App)
+});
