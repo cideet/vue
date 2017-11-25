266783da190301cc5711ee6bdddf3005192a9f22
zhangsanfeng <123456@qq.com>
2017-11-26 07:07:28 +0800

    路由、api均OK

---
+++ music3/src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import HelloWorld1 from '@/components/HelloWorld1'
+import HelloWorld2 from '@/components/HelloWorld2'
+
+Vue.use(Router)
+
+export default new Router({
+    routes: [
+        {
+            path: '/',
+            name: 'HelloWorld',
+            component: HelloWorld
+        },
+        {
+            path: '/h1',
+            name: 'HelloWorld1',
+            component: HelloWorld1
+        },
+        {
+            path: '/h2',
+            name: 'HelloWorld2',
+            component: HelloWorld2
+        }
+    ]
+})
