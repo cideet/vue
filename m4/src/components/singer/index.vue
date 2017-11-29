9b109cb83f35127ac7aab9283df2cabbafeea000
zhangsanfeng <123456@qq.com>
2017-11-29 11:38:12 +0800

    获取singer数据

--- m4/src/components/singer/index.vue
+++ m4/src/components/singer/index.vue
@@ -1,16 +1,39 @@
 <template>
     <div class="singer">
-        <div>singer</div>
-        <router-view></router-view>
+
     </div>
 </template>
 
 <script type="text/ecmascript-6">
-    export default{}
+    import {getSingerList} from 'api/singer.js';
+    import {ERR_OK} from 'api/config.js';
+    export default{
+        data(){
+            return {
+                singers: []
+            }
+        },
+        created(){
+            this._getSingerList();
+        },
+        methods: {
+            _getSingerList(){
+                getSingerList().then((res)=> {
+                    //console.log(res);
+                    if (res.code == ERR_OK) {
+                        this.singers = res.data.list;
+                    }
+                });
+            }
+        }
+    }
 </script>
 
 <style lang="less" rel="stylesheet/less">
-    .c {
-        color: white;
+    .singer {
+        position: fixed;
+        top: 80px;
+        bottom: 0;
+        width: 100%;
     }
 </style>
