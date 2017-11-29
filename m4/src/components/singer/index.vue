ec1fda24f7f42bc6ddbbfd25aa4c3200eec0ca91
zhangsanfeng <123456@qq.com>
2017-11-29 12:26:54 +0800

    singer数据处理

--- m4/src/components/singer/index.vue
+++ m4/src/components/singer/index.vue
@@ -7,6 +7,10 @@
 <script type="text/ecmascript-6">
     import {getSingerList} from 'api/singer.js';
     import {ERR_OK} from 'api/config.js';
+
+    const HOT_NAME = '热门';
+    const HOT_SINGER_LEN = 10;
+
     export default{
         data(){
             return {
@@ -22,9 +26,57 @@
                     //console.log(res);
                     if (res.code == ERR_OK) {
                         this.singers = res.data.list;
+                        console.log(this._normalizeSinger(this.singers));
                     }
                 });
+            },
+
+            _normalizeSinger(list) {
+                let map = {
+                    hot: {
+                        title: HOT_NAME,
+                        items: []
+                    }
+                };
+                list.forEach((item, index) => {
+                    if (index < HOT_SINGER_LEN) {
+                        map.hot.items.push({
+                            id: item.Fsinger_mid,
+                            name: item.Fsinger_name,
+                            avatar: `https://y.gtimg.cn/music/photo_new/T001R300x300M000${item.Fsinger_mid}.jpg?max_age=2592000`
+                        });
+                    }
+                    const key = item.Findex;
+                    if (!map[key]) {
+                        map[key] = {
+                            title: key,
+                            items: []
+                        }
+                    }
+                    map[key].items.push({
+                        id: item.Fsinger_mid,
+                        name: item.Fsinger_name,
+                        avatar: `https://y.gtimg.cn/music/photo_new/T001R300x300M000${item.Fsinger_mid}.jpg?max_age=2592000`
+                    });
+                });
+                // 为了得到有序列表 处理map
+                let hot = [];
+                let ret = [];
+                for (let key in map) {
+                    let val = map[key];
+                    if (val.title.match(/[a-zA-Z]/)) {
+                        ret.push(val);
+                    } else if (val.title === HOT_NAME) {
+                        hot.push(val);
+                    }
+                }
+                ret.sort((a, b) => {
+                    return a.title.charCodeAt(0) - b.title.charCodeAt(0);
+                });
+                return hot.concat(ret);
             }
+
+
         }
     }
 </script>
