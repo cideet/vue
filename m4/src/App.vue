0f0ab7f798b690f96393ca8c5530922bf78ec025
zhangsanfeng <123456@qq.com>
2017-12-02 07:45:02 +0800

    页面载入播放器，并设置隐藏

--- m4/src/App.vue
+++ m4/src/App.vue
@@ -5,16 +5,19 @@
         <keep-alive>
             <router-view></router-view>
         </keep-alive>
+        <v-player></v-player>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
     import MHeader from 'components/m-header/index.vue';
     import Tab from 'components/tab/index.vue';
+    import Player from 'components/player/index.vue';
     export default{
         components: {
             'v-m-header': MHeader,
-            'v-tab': Tab
+            'v-tab': Tab,
+            'v-player': Player
         }
 
     }
