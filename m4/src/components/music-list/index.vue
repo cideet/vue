a7b3e57473de939b07ca0b6506bb374d4211df9c
zhangsanfeng <123456@qq.com>
2017-12-05 04:35:12 +0800

    随机播放

--- m4/src/components/music-list/index.vue
+++ m4/src/components/music-list/index.vue
@@ -6,7 +6,9 @@
         <h1 class="title" v-html="title"></h1>
         <div class="bg-image" :style="bgStyle" ref="bgImage">
             <div class="play-wrapper">
-                <div class="play" v-show="songs.length>0" ref="playBtn">
+                <div class="play" v-show="songs.length>0"
+                     @click="random"
+                     ref="playBtn">
                     <i class="icon-play"></i>
                     <span class="text">随机播放全部</span>
                 </div>
@@ -63,6 +65,9 @@
             this.$refs.list.$el.style.top = this.imageHeight + 'px';
         },
         methods: {
+            random(){
+                this.randomPlay({list: this.songs});
+            },
             scroll(pos){
                 this.scrollY = pos.y;
                 // console.log(this.scrollY);
@@ -74,7 +79,8 @@
                 this.selectPlay({list: this.songs, index: index});
             },
             ...mapActions([
-                'selectPlay'
+                'selectPlay',
+                'randomPlay'
             ])
         },
         watch: {
--- m4/src/store/actions.js
+++ m4/src/store/actions.js
@@ -5,6 +5,8 @@
 //另一种是对mutation的封装
 
 import * as types from './mutation-types.js';
+import {playMode} from 'common/js/config.js';
+import {shuffle} from'common/js/util.js';
 
 /**
  * 选择播放
@@ -18,8 +20,29 @@ import * as types from './mutation-types.js';
  */
 export const selectPlay = function ({commit, state}, {list, index}) {
     commit(types.SET_SEQUENCE_LIST, list);      //播放顺序列表
-    commit(types.SET_PLAY_LIST, list);           //播放的音乐列表
+    if (state.mode == playMode.random) {
+        let randomList = shuffle(list);
+        commit(types.SET_PLAY_LIST, randomList);
+        index = findIndx(randomList, list[index]);
+    } else {
+        commit(types.SET_PLAY_LIST, list);           //播放的音乐列表
+    }
     commit(types.SET_CURRENT_INDEX, index);     //当前播放的索引
     commit(types.SET_FULL_SCREEN, true);        //是否全屏
     commit(types.SET_PLAYING_STATE, true);      //播放状态
 };
+
+export const randomPlay = function ({commit}, {list}) {
+    commit(types.SET_PLAY_MODE, playMode.random);
+    commit(types.SET_SEQUENCE_LIST, list);
+    commit(types.SET_PLAY_LIST, shuffle(list));
+    commit(types.SET_CURRENT_INDEX, 0);
+    commit(types.SET_FULL_SCREEN, true);
+    commit(types.SET_PLAYING_STATE, true);
+};
+
+function findIndx(list, song) {
+    return list.findIndex((item)=> {
+        return item.id == song.id;
+    })
+}
