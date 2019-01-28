957d07f09da9e53919e2f92acf530e78b8053840
张三丰 <123456@qq.com>
2019-01-28 10:20:38 +0800

    安装less

--- vue3/src/components/HelloWorld.vue
+++ vue3/src/components/HelloWorld.vue
@@ -50,7 +50,7 @@
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="less" rel="stylesheet/less">
     h1, h2 {
         font-weight: normal;
     }
@@ -58,11 +58,10 @@
     ul {
         list-style-type: none;
         padding: 0;
-    }
-
-    li {
-        display: inline-block;
-        margin: 0 10px;
+        li {
+            display: inline-block;
+            margin: 0 10px;
+        }
     }
 
     a {
